e1362f77946fd944937646293fc7ff79698babf5
Taehyeong Gwon <xogud0930@gmail.com>
2021-05-21 17:57:36 +0900

    port 변경

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const app = express()
-const port = 6050
+const port = process.env.PORT || 6050;
 
 const bodyParser = require('body-parser')
 const { User } = require('./models/User')
